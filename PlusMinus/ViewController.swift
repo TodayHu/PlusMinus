1bd2611c77b07174330cc367b331b4ddf3f0536f
nickmc1210 <nickmc1210@gmail.com>
2015-04-08 22:34:11 -0700

    Initial Commit

---
+++ PlusMinus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlusMinus
+//
+//  Created by Nick McCardel on 4/8/15.
+//  Copyright (c) 2015 Bleu Haus Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
